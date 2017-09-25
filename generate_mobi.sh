d9c2ace1b649707e95826a8034dfc338b25d19fe
Petr "Stone" Hracek <phracek@redhat.com>
2017-09-25 13:59:55 +0200

    Generate all versions
    Signed-off-by: Petr "Stone" Hracek <phracek@redhat.com>

--- generate_mobi.sh
+++ generate_mobi.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
 
-~/kindlegen mobi/dokumenty_II_vat_koncilu.opf
+~/kindlegen mobi/dokumenty_II_vat_koncilu_cs.opf
+~/kindlegen mobi/dokumenty_II_vat_koncilu_en.opf
+~/kindlegen mobi/dokumenty_II_vat_koncilu_lt.opf
